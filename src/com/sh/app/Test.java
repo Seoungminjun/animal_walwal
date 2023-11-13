16922eea597e7caac60916225a55e74b05cb48fe
Seoungminjun <dkwj000@naver.com>
2023-11-13 19:35:35 +0900

    Hello 출력문 소문자에서 대문자로 변경

--- src/com/sh/app/Test.java
+++ src/com/sh/app/Test.java
@@ -3,9 +3,9 @@ package com.sh.app;
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world");
-		System.out.println("Hello Git");
-		
+		System.out.println("HELLO WORLD");
+		System.out.println("HELLO GIT");
+
 		new Dog().bark();
 		new Cat().jump();
 		new Tiger().attack();
