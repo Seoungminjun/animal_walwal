d2039cb76394767b66d32cffd81d8bd9fcda90fe
Seoungminjun <dkwj000@naver.com>
2023-11-13 19:16:23 +0900

    Cat 클래스 추가 출력문 고양이 점프하다 추가

---
+++ src/com/sh/app/Cat.java
@@ -0,0 +1,9 @@
+package com.sh.app;
+
+public class Cat {
+
+	public void jump() {
+		System.out.println("고양이 점프하다.");
+	}
+
+}
--- src/com/sh/app/Test.java
+++ src/com/sh/app/Test.java
@@ -5,6 +5,9 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 		System.out.println("Hello Git");
+		
+		new Dog().bark();
+		new Cat().jump();
 	}
 
 }
